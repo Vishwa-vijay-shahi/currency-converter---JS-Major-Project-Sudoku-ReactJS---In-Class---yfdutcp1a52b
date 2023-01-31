726f93e4e617e25393729d2f3f9b37f6a9cba12d
Vishwa-vijay-shahi <115209572+Vishwa-vijay-shahi@users.noreply.github.com>
2023-01-31 10:50:22 +0530

    Update CurrencyConverter.js

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,12 +1,6 @@
-import React from 'react'
 import React, { useEffect, useState } from "react";
 
 export default function CurrencyConverter() {
-    return (
-        <div>
-
-        </div>
-    )
   let currencies = [
     "CAD",
     "HKD",
